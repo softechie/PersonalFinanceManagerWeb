c620e58098c71130bea5964bb519bb367b606d28
Nagarjuna <nagarjuna993@hotmail.com>
2019-04-06 00:53:41 +0530

    Revert "React JS structure and Partial implementation of Income module"

--- src/webapp/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const axiosInstance = axios.create({
-  baseURL: 'https://localhost:8443/v1',
-  // baseURL: 'https://pfm-api.cfapps.io/v1',
-  /* other custom settings */
-})
-
-export default axiosInstance
