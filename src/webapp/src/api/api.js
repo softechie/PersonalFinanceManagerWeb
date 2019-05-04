3cb55a6d28a7b229035405078acf7282f7d34bb8
Abdelhalim <golden-boy1994@hotmail.fr>
2019-05-03 22:56:14 -0700

    added dot env

--- src/webapp/src/api/api.js
+++ src/webapp/src/api/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API | 'https://localhost:8443/v1',
+  baseURL: process.env.REACT_APP_API || 'https://localhost:8443/v1',
   // baseURL: 'https://pfm-api.cfapps.io/v1',
   /* other custom settings */
 })
